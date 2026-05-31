44b4ea24d0a5362af1ce16ba3b47f02cd50c5942
melodypapa <melodypapa@outlook.com>
2026-05-31 09:26:44 +0800

    feat: export ClangParser from parsers module

--- src/autosar_calltree/parsers/__init__.py
+++ src/autosar_calltree/parsers/__init__.py
@@ -2,5 +2,6 @@
 
 from .autosar_parser import AutosarParser
 from .c_parser import CParser
+from .clang_parser import ClangParser
 
-__all__ = ["AutosarParser", "CParser"]
+__all__ = ["AutosarParser", "CParser", "ClangParser"]
